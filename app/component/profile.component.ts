f29a5d466e1874a6dec462e95bf046e8098d58b8
Supamiu <contact@flavien-normand.fr>
2016-09-23 15:22:03 +0200

    Mise en place d'une base pour la partie profile.

---
+++ app/component/profile.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "profile",
+    template: ``,
+    styleUrls: ["profile.component.css"]
+})
+export class ProfileComponent {
+
+    constructor(private cvService:CVService){}
+}
